2c92af9feba29fea93d62647bffe74bc23f5b19c
muxammedtm <33852486+muxammedtm@users.noreply.github.com>
2026-06-01 23:21:44 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    BOT_TOKEN    = os.getenv("BOT_TOKEN", "8621461817:AAG1tZqIsedLNmebgrIvEcjhQwNQRcywxTQ")
+    BOT_TOKEN    = os.getenv("BOT_TOKEN", "8621461817:AAHT3HBWe7ljJ64uDE13U1eDPVjmdiw1hVg")
     OWNER_ID     = int(os.getenv("OWNER_ID", "610489050"))
     DB_PATH      = os.getenv("DB_PATH", "kinobot.db")
     POST_CHANNEL = os.getenv("POST_CHANNEL", "")  # Admin paneldan o'rnatiladi
